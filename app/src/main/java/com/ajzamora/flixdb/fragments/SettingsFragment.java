e7a5244d8c5835f2ca92c40f52f80d2507c8b535
zam <ajmzamora@gmail.com>
2020-04-11 06:50:58 -0400

    feat(preferences): implement OnSharedPreferenceChangeListener
    register and unregister onsharedpreferences, add summary onCreate and onChange

--- app/src/main/java/com/ajzamora/flixdb/fragments/SettingsFragment.java
+++ app/src/main/java/com/ajzamora/flixdb/fragments/SettingsFragment.java
@@ -1,15 +1,86 @@
 package com.ajzamora.flixdb.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
 import com.ajzamora.flixdb.R;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat
+    implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.pref_general);
+
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        int count = prefScreen.getPreferenceCount();
+
+        for (int i = 0; i < count; i++) {
+            Preference preference = prefScreen.getPreference(i);
+            if (preference instanceof PreferenceCategory) {
+                PreferenceCategory preferenceGroup = (PreferenceCategory) preference;
+                int groupItemPreferenceCount = preferenceGroup.getPreferenceCount();
+                for (int j = 0; j < groupItemPreferenceCount; j++) {
+                    Preference currentItemPreference = preferenceGroup.getPreference(j);
+                    String value = sharedPreferences.getString(currentItemPreference.getKey(), "");
+                    setPreferenceSummary(currentItemPreference, value);
+                }
+            } else {
+                String value = sharedPreferences.getString(preference.getKey(), "");
+                setPreferenceSummary(preference, value);
+            }
+
+        }
+    }
+
+    private void setPreferenceSummary(Preference preference, Object value) {
+        if (preference instanceof CheckBoxPreference) return;
+        String stringValue = value.toString();
+
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+            preference.setSummary(stringValue);
+        }
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        /* Unregister the preference change listener */
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        /* Register the preference change listener */
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        if (null != preference) {
+            if (!(preference instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(preference.getKey(), "");
+                setPreferenceSummary(preference, value);
+            }
+        }
     }
 }
