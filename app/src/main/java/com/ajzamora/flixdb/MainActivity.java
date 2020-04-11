611f88722e5890cd9b4bc691f91ec9066cfba885
zam <ajmzamora@gmail.com>
2020-04-11 05:48:42 -0400

    feat(menu): add menu for settings

--- app/src/main/java/com/ajzamora/flixdb/MainActivity.java
+++ app/src/main/java/com/ajzamora/flixdb/MainActivity.java
@@ -1,10 +1,14 @@
 package com.ajzamora.flixdb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.ajzamora.flixdb.adapters.MovieAdapter;
 import com.ajzamora.flixdb.utils.LayoutUtils;
@@ -31,5 +35,22 @@ public class MainActivity extends AppCompatActivity {
 
         mMovieAdapter = new MovieAdapter(NUM_LIST_ITEMS);
         mMainRV.setAdapter(mMovieAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent startSettingsActivity = new Intent(this, SettingsActivity.class);
+            startActivity(startSettingsActivity);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
