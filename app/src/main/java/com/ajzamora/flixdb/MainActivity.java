be4d9111f7997460c2e70b95107b852c28293b96
zam <ajmzamora@gmail.com>
2020-04-12 16:30:46 -0400

    feat(main): implement OnSharedPreferenceChangeListener

--- app/src/main/java/com/ajzamora/flixdb/MainActivity.java
+++ app/src/main/java/com/ajzamora/flixdb/MainActivity.java
@@ -2,29 +2,38 @@ package com.ajzamora.flixdb;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.ajzamora.flixdb.adapters.MovieAdapter;
 import com.ajzamora.flixdb.utils.LayoutUtils;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
 
+    public final String LOG_TAG = MainActivity.class.getSimpleName();
     private static final int NUM_LIST_ITEMS = 100;
 
     private MovieAdapter mMovieAdapter;
     private RecyclerView mMainRV;
+
+    private static boolean sharedPreferenceFlag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initUI();
+
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
     }
 
     public void initUI() {
@@ -52,5 +61,28 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        sharedPreferenceFlag = true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (sharedPreferenceFlag) {
+            Log.i(LOG_TAG, "onStart: preferences were updated");
+            sharedPreferenceFlag = false;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
     }
 }
