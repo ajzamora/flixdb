363cca7f4c8cc0900e08e650c305bec9aff3ac2b
zam <ajmzamora@gmail.com>
2020-04-23 06:08:55 -0400

    feat(detail): load real trailer data using loader and remove temp data
    remove set hide on content scroll enabled
    use AppCompatDelegate  instead of Window for action bar overlay
    return intent result to save on movie adapter

--- app/src/main/java/com/ajzamora/flixdb/DetailActivity.java
+++ app/src/main/java/com/ajzamora/flixdb/DetailActivity.java
@@ -1,30 +1,46 @@
 package com.ajzamora.flixdb;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.app.NavUtils;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ajzamora.flixdb.adapters.TrailerAdapter;
+import com.ajzamora.flixdb.loaders.TrailerLoader;
+import com.ajzamora.flixdb.models.FlixPreferences;
 import com.ajzamora.flixdb.models.Movie;
 import com.ajzamora.flixdb.models.Trailer;
+import com.ajzamora.flixdb.utils.NetworkUtils;
 import com.squareup.picasso.Picasso;
 
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<List<Trailer>> {
+    public static final int REQUEST_CODE = 2;
+    public static final String EXTRA_TRAILERS = "extra_trailers";
     public static final String EXTRA_MOVIE = "extra_movie";
+
+    private static final int TRAILER_LOADER_ID = 2;
 
     private TrailerAdapter mTrailerAdapter;
     private RecyclerView mDetailRV;
@@ -35,10 +51,14 @@ public class DetailActivity extends AppCompatActivity {
     private TextView mRatingTV;
     private TextView mDateTV;
 
+    private String mId;
+    private int mMoviePosition;
+    private Movie mMovie;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        supportRequestWindowFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
+        supportRequestWindowFeature(AppCompatDelegate.FEATURE_SUPPORT_ACTION_BAR_OVERLAY);
         setContentView(R.layout.activity_detail);
 
         initUI();
@@ -47,16 +67,28 @@ public class DetailActivity extends AppCompatActivity {
             closeOnError();
         }
 
-        Movie movie = getIntent().getParcelableExtra(EXTRA_MOVIE);
-        if (movie != null) {
-            populateUI(movie);
-        }
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayShowTitleEnabled(false);
-            actionBar.setHideOnContentScrollEnabled(true);
             actionBar.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
             actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        mMovie = getIntent().getParcelableExtra(EXTRA_MOVIE);
+        mMoviePosition = getIntent().getIntExtra(MainActivity.EXTRA_MOVIE_POS, MainActivity.EXTRA_MOVIE_POS_DEFAULT);
+
+        if (mMovie != null) {
+            mId = mMovie.getId();
+            populateUI(mMovie);
+            if (mMovie.getTrailers() != null && !mMovie.getTrailers().isEmpty()) {
+                mTrailerAdapter.setData(mMovie.getTrailers());
+            }
+        }
+        if (isOnline()) {
+            LoaderManager loaderManager = getSupportLoaderManager();
+            loaderManager.initLoader(TRAILER_LOADER_ID, null, this);
+        } else {
+            // no internet connection
         }
     }
 
@@ -64,7 +96,7 @@ public class DetailActivity extends AppCompatActivity {
         mDetailRV = findViewById(R.id.rv_trailer_detail);
         mDetailRV.setLayoutManager(new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false));
         mDetailRV.setHasFixedSize(true);
-        mTrailerAdapter = new TrailerAdapter(generateSampleData());
+        mTrailerAdapter = new TrailerAdapter(new ArrayList<Trailer>());
         mDetailRV.setAdapter(mTrailerAdapter);
 
         mThumbIV = findViewById(R.id.iv_thumbnail_detail);
@@ -73,23 +105,6 @@ public class DetailActivity extends AppCompatActivity {
         mPlotTV = findViewById(R.id.tv_plot_detail);
         mRatingTV = findViewById(R.id.tv_rating_detail);
         mDateTV = findViewById(R.id.tv_date_detail);
-    }
-
-    private List<Trailer> generateSampleData() {
-        List<Trailer> trailers = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            Trailer.Builder trailerBuilder = new Trailer.Builder();
-            String id = String.valueOf(i);
-            String key = String.valueOf(i);
-            String site = "youtube";
-
-            trailerBuilder.id(id)
-                    .key(key)
-                    .site(site);
-            trailers.add(trailerBuilder.build());
-        }
-
-        return trailers;
     }
 
     private void closeOnError() {
@@ -116,10 +131,55 @@ public class DetailActivity extends AppCompatActivity {
         int id = item.getItemId();
         switch (id) {
             case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
+                finish();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    public void finish() {
+        Intent intent = NavUtils.getParentActivityIntent(this);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra(EXTRA_TRAILERS, mTrailerAdapter.getData());
+        intent.putExtra(MainActivity.EXTRA_MOVIE_POS, mMoviePosition);
+        setResult(RESULT_OK, intent);
+        NavUtils.navigateUpTo(this, intent);
+        super.finish();
+    }
+
+    @NonNull
+    @Override
+    public Loader<List<Trailer>> onCreateLoader(int id, @Nullable Bundle args) {
+        String api = FlixPreferences.getPreferredAPI(this);
+
+        return new TrailerLoader(this, api, mId);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<List<Trailer>> loader, List<Trailer> trailers) {
+        mTrailerAdapter.clear();
+        if (trailers != null && !trailers.isEmpty()) {
+            mTrailerAdapter.setData(trailers);
+        } else {
+            if (NetworkUtils.STATUS_CODE == HttpURLConnection.HTTP_UNAUTHORIZED) {
+                // Invalid Api Key
+            } else {
+                // No Trailer Found
+            }
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<List<Trailer>> loader) {
+        mTrailerAdapter.clear();
+    }
+
+    private boolean isOnline() {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
     }
 }
