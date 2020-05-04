d0abe43fdcf0ce9624309a3f1bbafb53545d3878
zam <ajmzamora@gmail.com>
2020-05-04 05:12:07 -0400

    refactor(loader): use cursor loader and add movie sync observer
    delete loader
    temporary trigger sync on item click

--- app/src/main/java/com/ajzamora/flixdb/MainActivity.java
+++ app/src/main/java/com/ajzamora/flixdb/MainActivity.java
@@ -3,43 +3,47 @@ package com.ajzamora.flixdb;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.ContentObserver;
+import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ContentLoadingProgressBar;
 import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
 import androidx.loader.content.Loader;
 import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ajzamora.flixdb.adapters.MovieAdapter;
-import com.ajzamora.flixdb.loaders.MovieLoader;
 import com.ajzamora.flixdb.models.FlixPreferences;
 import com.ajzamora.flixdb.models.Movie;
+import com.ajzamora.flixdb.models.MovieContract.MovieEntry;
 import com.ajzamora.flixdb.models.Review;
 import com.ajzamora.flixdb.models.Trailer;
 import com.ajzamora.flixdb.sync.SyncAdapter;
 import com.ajzamora.flixdb.utils.AccountUtils;
 import com.ajzamora.flixdb.utils.LayoutUtils;
-import com.ajzamora.flixdb.utils.NetworkUtils;
 
-import java.net.HttpURLConnection;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements SharedPreferences.OnSharedPreferenceChangeListener,
-        LoaderManager.LoaderCallbacks<List<Movie>>,
+        LoaderManager.LoaderCallbacks<Cursor>,
         MovieAdapter.RecyclerItemClickListener {
 
     public final String LOG_TAG = MainActivity.class.getSimpleName();
@@ -52,7 +56,24 @@ public class MainActivity extends AppCompatActivity
     private TextView mEmptyStateTV;
     private ImageView mEmptyIconIV;
     private TextView mEmptyIconLabelTV;
-    private ProgressBar mIndicatorPB;
+    private ContentLoadingProgressBar mIndicatorPB;
+    private MovieObserver movieObserver;
+
+    /**
+     * Example content observer for observing article data changes.
+     */
+    private final class MovieObserver extends ContentObserver {
+        private MovieObserver() {
+            // Ensure callbacks happen on the UI thread
+            super(new Handler(Looper.getMainLooper()));
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            // Handle your data changes here!!!
+            refreshMovies();
+        }
+    }
 
     private static boolean sharedPreferenceFlag = false;
 
@@ -62,21 +83,15 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         initUI();
 
-        if (isOnline()) {
-            LoaderManager loaderManager = getSupportLoaderManager();
-            loaderManager.initLoader(MOVIE_LOADER_ID, null, this);
-        } else {
-            setEmptyState(R.string.empty_state_no_internet, R.drawable.ic_panda, R.string.empty_state_icon_panda);
-        }
-
         PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
 
         // Create your sync account
         AccountUtils.createSyncAccount(this);
 
-        // Perform a manual sync by calling this:
-        SyncAdapter.performSync();
+        movieObserver = new MovieObserver();
 
+        LoaderManager loaderManager = getSupportLoaderManager();
+        loaderManager.initLoader(MOVIE_LOADER_ID, null, this);
     }
 
     public void initUI() {
@@ -94,8 +109,6 @@ public class MainActivity extends AppCompatActivity
     }
 
     private void setEmptyState(int resEmptyStateText, int resEmptyIcoImage, int resEmptyStateIcoLabel) {
-        mIndicatorPB.setVisibility(View.GONE);
-
         mEmptyStateTV.setVisibility(View.VISIBLE);
         mEmptyIconIV.setVisibility(View.VISIBLE);
         mEmptyIconLabelTV.setVisibility(View.VISIBLE);
@@ -108,8 +121,6 @@ public class MainActivity extends AppCompatActivity
         mEmptyStateTV.setVisibility(View.GONE);
         mEmptyIconIV.setVisibility(View.GONE);
         mEmptyIconLabelTV.setVisibility(View.GONE);
-
-        mIndicatorPB.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -137,15 +148,25 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onStart() {
         super.onStart();
+        // Register the observer at the start of our activity
+        getContentResolver().registerContentObserver(MovieEntry.CONTENT_URI, true, movieObserver);
 
         if (sharedPreferenceFlag && isOnline()) {
             Log.i(LOG_TAG, "onStart: preferences were updated");
+            mMovieAdapter.swapCursor(null);
             hideEmptyState();
-
-            mMovieAdapter.clear();
+            mIndicatorPB.show();
             getSupportLoaderManager().restartLoader(MOVIE_LOADER_ID, null, this);
             sharedPreferenceFlag = false;
         }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Unregister the observer at the stop of our activity
+        if (movieObserver != null) getContentResolver().unregisterContentObserver(movieObserver);
     }
 
     @Override
@@ -155,33 +176,51 @@ public class MainActivity extends AppCompatActivity
                 .unregisterOnSharedPreferenceChangeListener(this);
     }
 
+
     @NonNull
     @Override
-    public Loader<List<Movie>> onCreateLoader(int id, @Nullable Bundle args) {
-        String api = FlixPreferences.getPreferredAPI(this);
-        String sortOrder = FlixPreferences.getPreferredSortOrder(this);
-
-        return new MovieLoader(this, api, sortOrder);
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        String sortType = "DESC";
+        String sortOrder = (FlixPreferences.isSortPopular(this) ? MovieEntry.COLUMN_MOVIE_POPULARITY : MovieEntry.COLUMN_MOVIE_RATING).concat(" ").concat(sortType);
+        Log.v(LOG_TAG, "onCreateLoader ".concat(sortOrder));
+        return new CursorLoader(this,
+                MovieEntry.CONTENT_URI,
+                null,
+                null,
+                null,
+                sortOrder);
     }
 
+
     @Override
-    public void onLoadFinished(@NonNull Loader<List<Movie>> loader, List<Movie> movies) {
-        mIndicatorPB.setVisibility(View.GONE);
-        mMovieAdapter.clear();
-        if (movies != null && !movies.isEmpty()) {
-            mMovieAdapter.setData(movies);
-        } else {
-            if (NetworkUtils.STATUS_CODE == HttpURLConnection.HTTP_UNAUTHORIZED) {
-                setEmptyState(R.string.empty_state_api_invalid, R.drawable.ic_smart_key, R.string.empty_state_api_instructions);
-            } else {
-                setEmptyState(R.string.empty_state_no_movies, R.drawable.ic_sad, R.string.empty_state_icon_sad);
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+        Log.v(LOG_TAG, "onLoadFinished");
+
+        mIndicatorPB.hide();
+        if (data.getCount() > 0) {
+            mMovieAdapter.swapCursor(data);
+            Log.v(LOG_TAG, "onLoadFinished: if" + data.getCount() + " " + mMovieAdapter.getItemCount());
+            StringBuilder sb = new StringBuilder();
+            int count = data.getCount();
+            while (data.moveToNext()) {
+                int titleColIdx = data.getColumnIndex(MovieEntry.COLUMN_MOVIE_TITLE);
+                int rateColIdx = data.getColumnIndex(MovieEntry.COLUMN_MOVIE_RATING);
+                String title = data.getString(titleColIdx);
+                String rate = data.getString(rateColIdx);
+                sb.append(title).append(" ").append(rate).append("\n");
             }
+            sb.append("total count: ").append(count);
+            Log.v(LOG_TAG, sb.toString());
+        } else {
+            setEmptyState(R.string.empty_state_no_movies, R.drawable.ic_sad, R.string.empty_state_icon_sad);
+            Log.v(LOG_TAG, "onLoadFinished: else");
         }
     }
 
     @Override
-    public void onLoaderReset(@NonNull Loader<List<Movie>> loader) {
-        mMovieAdapter.clear();
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        Log.v(LOG_TAG, "onLoadReset");
+        mMovieAdapter.swapCursor(null);
     }
 
     @Override
@@ -190,11 +229,14 @@ public class MainActivity extends AppCompatActivity
     }
 
     private void launchDetailActivity(int moviePosition) {
-        Movie currentMovie = mMovieAdapter.getMovieAt(moviePosition);
-        Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra(MainActivity.EXTRA_MOVIE_POS, moviePosition);
-        intent.putExtra(DetailActivity.EXTRA_MOVIE, currentMovie);
-        startActivityForResult(intent, DetailActivity.REQUEST_CODE);
+//        Movie currentMovie = mMovieAdapter.getMovieAt(moviePosition);
+//        Intent intent = new Intent(this, DetailActivity.class);
+//        intent.putExtra(MainActivity.EXTRA_MOVIE_POS, moviePosition);
+//        intent.putExtra(DetailActivity.EXTRA_MOVIE, currentMovie);
+//        startActivityForResult(intent, DetailActivity.REQUEST_CODE);
+        // TODO: delete temporary test sync
+        // Perform a manual sync by calling this:
+        SyncAdapter.performSync();
     }
 
     @Override
@@ -203,10 +245,10 @@ public class MainActivity extends AppCompatActivity
         if (requestCode == DetailActivity.REQUEST_CODE && resultCode == RESULT_OK) {
             int moviePosition = data.getIntExtra(MainActivity.EXTRA_MOVIE_POS, MainActivity.EXTRA_MOVIE_POS_DEFAULT);
             List<Trailer> trailers = data.getParcelableArrayListExtra(DetailActivity.EXTRA_TRAILERS);
-            mMovieAdapter.setTrailerListAt(moviePosition, trailers);
+//            mMovieAdapter.setTrailerListAt(moviePosition, trailers);
 
             List<Review> reviews = data.getParcelableArrayListExtra(DetailActivity.EXTRA_REVIEWS);
-            mMovieAdapter.setReviewListAt(moviePosition, reviews);
+//            mMovieAdapter.setReviewListAt(moviePosition, reviews);
         }
     }
 
@@ -215,5 +257,9 @@ public class MainActivity extends AppCompatActivity
                 getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         return (networkInfo != null && networkInfo.isConnected());
+    }
+
+    private void refreshMovies() {
+        Log.i(getClass().getName(), "Movies data has changed!");
     }
 }
